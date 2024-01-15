02488f04e432d20da32f6ec3a2cfdb84bacbfab3
Rudney Rodrigues <rudney.un2016@gmail.com>
2024-01-15 11:26:03 -0300

    :bricks: ci: Atualizando URL da API

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL:
-    'http://localhost:3000/api/' || `https://${process.env.VERCEL_URL}/api/`,
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 })
